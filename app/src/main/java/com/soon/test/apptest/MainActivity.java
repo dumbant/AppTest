2731663120aca2f43e40ac788e6a97b70450d814
songlin <zsl354310@163.com>
2018-11-11 20:18:46 +0800

    study android program
    apptest project

---
+++ app/src/main/java/com/soon/test/apptest/MainActivity.java
@@ -0,0 +1,19 @@
+package com.soon.test.apptest;
+
+import android.graphics.drawable.Drawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
+
+import java.lang.reflect.Field;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
